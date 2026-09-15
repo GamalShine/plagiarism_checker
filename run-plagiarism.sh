0794ac3d7c6c3fb1a2e36435287bd6b98f4da80e
GamalShine <119243187+GamalShine@users.noreply.github.com>
2026-09-15 12:41:42 +0700

    Log queue cron start time

--- run-plagiarism.sh
+++ run-plagiarism.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') Cron started" >> /home/u704951863/queue.log
+
 cd /home/u704951863/domains/naskahcek.com/public_html || exit 1
 
 /usr/bin/php artisan queue:work database --queue=plagiarism --once --tries=1 --timeout=300 --verbose >> /home/u704951863/queue.log 2>&1
