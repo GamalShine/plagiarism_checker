2324e21a2e42665ea06a4b5f60cb88e37aacdc3b
GamalShine <119243187+GamalShine@users.noreply.github.com>
2026-09-15 12:29:19 +0700

    Update Hostinger project path

--- run-plagiarism.sh
+++ run-plagiarism.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd /home/u704951863/public_html || exit 1
+cd /home/u704951863/domains/naskahcek.com/public_html || exit 1
 
 /usr/bin/php artisan queue:work database --queue=plagiarism --once --tries=1 --timeout=300 --verbose >> /home/u704951863/queue.log 2>&1
